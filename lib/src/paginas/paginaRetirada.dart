261f886a52fdd6aa2738fcf04726c706e35ae895
Inmaculada Moran Rastrollo <imoran@ONDUPACK.local>
2022-06-08 10:17:59 +0200

    Añadido de funcionalidad al apartado de pendiente de retirada

--- lib/src/paginas/paginaRetirada.dart
+++ lib/src/paginas/paginaRetirada.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
+import 'package:solucionutiles/src/modelos/almacen.dart';
+import 'package:solucionutiles/src/modelos/maquina.dart';
 import 'package:solucionutiles/src/utils/utils.dart';
+import 'package:solucionutiles/src/widgets/menuNavegacion.dart';
 
 class PaginaRetirada extends StatefulWidget {
   const PaginaRetirada({Key? key}) : super(key: key);
@@ -11,28 +14,36 @@ class PaginaRetirada extends StatefulWidget {
 }
 
 class _PaginaRetiradaState extends State<PaginaRetirada> {
-  final opciones = [
+  String? opcionSeleccionada;
+  List<Almacen>? misAlmacenes;
+  List<Maquina>? misMaquinas;
+  List<String> opciones = [
     'Uno',
     'Dos',
     'Tres',
     'Cuatro',
-    'Uno',
-    'Dos',
-    'Tres',
-    'Cuatro',
-    'Uno',
-    'Dos',
-    'Tres',
-    'Cuatro',
-    'Uno',
-    'Dos',
-    'Tres',
-    'Cuatro'
+    'Cinco',
+    'Seis',
+    'Siete',
+    'Ocho',
+    'Nueve',
+    'Diez',
+    'Once',
+    'Doce',
   ];
   @override
   Widget build(BuildContext context) {
+    Map<dynamic, dynamic>? parametros =
+        ModalRoute.of(context)!.settings.arguments as Map?;
+    opcionSeleccionada = parametros!['opcionSeleccionada'] ?? TCliche;
+    misAlmacenes = parametros['misAlmacenes'];
+    misMaquinas = parametros['misMaquinas'];
     return Scaffold(
-      // drawer: MenuNavegacion(),
+      drawer: MenuNavegacion(
+        opcionSeleccionada: opcionSeleccionada,
+        misAlmacenes: misAlmacenes,
+        misMaquinas: misMaquinas,
+      ),
       appBar: dameAppBar('Pediente de retirada', context),
       body: ListView(children: _crearItems()),
     );
@@ -48,7 +59,7 @@ class _PaginaRetiradaState extends State<PaginaRetirada> {
             trailing: const Text('P: 4 C:330'),
             //Al hacer click que salga un dialogo que nos pregunte si deseamos eliminar de la lista el elemento
             onTap: () {
-              _alerta(context);
+              _alerta(context, item);
             },
           ),
           const Divider()
@@ -56,32 +67,36 @@ class _PaginaRetiradaState extends State<PaginaRetirada> {
       );
     }).toList();
   }
-}
 
-void _alerta(BuildContext context) {
-  showDialog(
-    context: context,
-    //Si clickamos fuera del cuadro este no se cerrara
-    barrierDismissible: false,
-    builder: (context) {
-      return AlertDialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        title: const Text('Eliminar elemento'),
-        content: const Text(
-            '¿Esta seguro de que desea eliminar este elemento de la lista?'),
-        actions: <Widget>[
-          FlatButton(
-            onPressed: () {},
-            child: const Text('Borrar'),
+  void _alerta(BuildContext context, String item) {
+    showDialog(
+      context: context,
+      //Si clickamos fuera del cuadro este no se cerrara
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
           ),
-          FlatButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text('Cancelar'),
-          ),
-        ],
-      );
-    },
-  );
+          title: const Text('Eliminar elemento'),
+          content: Text(
+              '¿Esta seguro de que desea eliminar el elemento $item de la lista?'),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () {
+                opciones.remove(item);
+                setState(() {});
+                Navigator.of(context).pop();
+              },
+              child: const Text('Borrar'),
+            ),
+            FlatButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cancelar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
